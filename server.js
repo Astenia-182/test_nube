6a25daf4297bce20aaa9594c7a5abe5a7ac869ef
angel <gaam.182@hotmail.com>
2023-08-07 18:29:36 -0600

    modifique a los tqm padres

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res)=> res.send('Hola')) 
+app.get('/', (req, res)=> res.send('los quiero padres')) 
 
 app.listen(3000)
 console.log("hola")
